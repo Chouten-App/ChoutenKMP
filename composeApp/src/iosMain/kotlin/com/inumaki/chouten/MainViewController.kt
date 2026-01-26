96cdceb1bf26dda7a1e0bc67d6a2ad63d3cbdba7
inumakieu <kempcompany05@gmail.com>
2026-01-26 14:32:07 +0100

    make ios gyro use remember

--- composeApp/src/iosMain/kotlin/com/inumaki/chouten/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/inumaki/chouten/MainViewController.kt
@@ -1,6 +1,7 @@
 package com.inumaki.chouten
 
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.runtime.remember
 
 fun MainViewController() = ComposeUIViewController {
     val headingProvider = remember { GyroProvider() }
